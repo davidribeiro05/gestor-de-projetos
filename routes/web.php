6f9e1c4f2ea8057c0cbe7a7132a741b33abdcf92
David Ribeiro <david.ribeiro05@gmail.com>
2021-01-12 00:04:13 -0300

    Criação das rotas para criar e atualizar projeto

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('manager.main', [
+        'title' => 'Bem vindo ao gestor de projetos'
+    ]);
 });
+
+Route::get('/projeto/criar', 'Project@create')
+    ->name('form_project');
+Route::post('/projeto', 'Project@store')
+    ->name('create_project');
+Route::get('/projeto/editar/{id}', 'Project@edit')
+    ->name('edit_project');
+Route::post('/projeto/editar', 'Project@update')
+    ->name('update_project');
