7edcdde46c3e5e9dc9aff57f665e07141e5b82b1
David Ribeiro <david.ribeiro05@gmail.com>
2021-01-12 06:25:46 -0300

    Adição do controlador de Processos

---
+++ app/Http/Controllers/Process.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Process as ProcessModel;
+use App\Models\Project as ProjectModel;
+use App\Models\Status;
+use Illuminate\Http\Request;
+
+class Process extends Controller
+{
+    public function create(Request $request)
+    {
+        $statuses = $statuses = Status::all('status');
+
+        return view('manager.process.register', [
+            'title' => 'Adicionar processos',
+            'statuses' => $statuses,
+            'project' => ProjectModel::findOrFail($request->project_id)
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        ProcessModel::create($request->all());
+    }
+
+    public function edit(Request $request, int $id)
+    {
+        $statuses = $statuses = Status::all('status');
+        $process = ProcessModel::find($id);
+
+        return view('manager.process.register', [
+            'title' => 'Adicionar processos',
+            'statuses' => $statuses,
+            'process' => $process
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        ProcessModel::where('id', $request->id)
+            ->update($request->except('_token'));
+        return redirect()->route('edit_process', $request->id);
+    }
+}
